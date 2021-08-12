b516e4ec769e109658408054237863a2106ba958
Itachisann <88840797+Itachisann@users.noreply.github.com>
2021-08-12 16:50:16 +0200

    Update helper.py

--- userbot/plugins/helper.py
+++ userbot/plugins/helper.py
@@ -1,19 +1,3 @@
-# TG-UserBot - A modular Telegram UserBot script for Python.
-# Copyright (C) 2019  Kandarp <https://github.com/kandnub>
-#
-# TG-UserBot is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# TG-UserBot is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with TG-UserBot.  If not, see <https://www.gnu.org/licenses/>.
-
 
 import os.path
 import re
@@ -24,8 +8,6 @@ from userbot.utils.events import NewMessage
 
 
 plugin_category: str = "helper"
-link: str = "https://tg-userbot.readthedocs.io/en/latest/userbot/commands.html"
-chunk: int = 5
 split_exp: re.Pattern = re.compile(r'\||\/')
 
 
@@ -36,8 +18,6 @@ split_exp: re.Pattern = re.compile(r'\||\/')
 async def helper(event: NewMessage.Event) -> None:
     arg = event.matches[0].group(0)
     enabled, senabled = await solve_commands(client.commands)
-    disabled, sdisabled = await solve_commands(client.disabled_commands)
-    categories = client.commandcategories
     prefix = client.prefix or '.'
     if arg:
       text = "**🌍 Lista Comandi 🌍**\n\n"
