168ec98bfcfd8e501c922d7fd20c313c23b3fe61
Itachisann <88840797+Itachisann@users.noreply.github.com>
2021-08-12 16:42:04 +0200

    Update userdata.py

--- userbot/plugins/userdata.py
+++ userbot/plugins/userdata.py
@@ -1,19 +1,3 @@
-# TG-UserBot - A modular Telegram UserBot script for Python.
-# Copyright (C) 2019  Kandarp <https://github.com/kandnub>
-#
-# TG-UserBot is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# TG-UserBot is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with TG-UserBot.  If not, see <https://www.gnu.org/licenses/>.
-
 
 import io
 import PIL
@@ -23,7 +7,7 @@ from telethon.utils import get_display_name, get_peer_id
 from telethon.tl import functions, types
 
 from userbot import client, LOGGER
-from userbot.helper_funcs.parser import Parser
+from userbot.other_func.parser import Parser
 from userbot.utils.events import NewMessage
 
 
