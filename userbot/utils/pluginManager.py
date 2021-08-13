3161db946ef480b0cd8e43086d57106aa43295b4
Itachisann <88840797+Itachisann@users.noreply.github.com>
2021-08-13 16:39:39 +0200

    Update pluginManager.py

--- userbot/utils/pluginManager.py
+++ userbot/utils/pluginManager.py
@@ -1,19 +1,3 @@
-# TG-UserBot - A modular Telegram UserBot script for Python.
-# Copyright (C) 2019  Kandarp <https://github.com/kandnub>
-#
-# TG-UserBot is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# TG-UserBot is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with TG-UserBot.  If not, see <https://www.gnu.org/licenses/>.
-
 
 import asyncio
 import configparser
@@ -173,7 +157,6 @@ class PluginManager:
             self._import_plugin(name, path, content)
 
     def add_handlers(self) -> None:
-        """Apply event handlers to all the found callbacks."""
         for plugin in self.active_plugins:
             for callback in plugin.callbacks:
                 self.client.add_event_handler(callback.callback)
@@ -208,7 +191,6 @@ class PluginManager:
         return plugins
 
     def _resolve_repo(self) -> Tuple[Dict[str, str], Dict[str, str]]:
-        """Fetch all the files from a repository recusrively."""
         LOGGER.info("Fetching all the external plugins from git repos")
         plugins: Dict[str, str] = {}
         helpers: Dict[str, str] = {}
