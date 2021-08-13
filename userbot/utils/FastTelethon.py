e590dbe0e9f27517530345d856bdfe43f76f6cbc
Itachisann <88840797+Itachisann@users.noreply.github.com>
2021-08-13 16:33:40 +0200

    Update FastTelethon.py

--- userbot/utils/FastTelethon.py
+++ userbot/utils/FastTelethon.py
@@ -1,8 +1,4 @@
-"""
-> Based on parallel_file_transfer.py from mautrix-telegram,
-  with permission to distribute under the MIT license
-> Copyright (C) 2019 Tulir Asokan - https://github.com/tulir/mautrix-telegram
-"""
+
 import asyncio
 import math
 import hashlib
